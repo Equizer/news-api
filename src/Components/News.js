940e4c210793e0872464b8439764110789b72f44
Equizer <130571773+Equizer@users.noreply.github.com>
2023-11-01 02:06:21 +0530

    add router and categories in the app

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,67 +1,77 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner.js'
 
 export class News extends Component {
-  constructor() {
-    super();
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    apiKey: '7a53337011074519b2a9b032b4ed21b9',
+    category: 'general'
+  }
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    apiKey: PropTypes.string,
+    category: PropTypes.string,
+  }
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
       totalResults: 0,
     }
+    document.title = `${(this.props.category.slice(0, 1)).toUpperCase()}${this.props.category.slice(1)} - Newzilla`;
   }
-  
-  async componentDidMount() {
-    let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=7a53337011074519b2a9b032b4ed21b9&pageSize=21';
+
+  async updateNews() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true })
     let data = await fetch(url);
     let parsedData = await data.json();
-
     //following line will set the state of article to the article in the url we get from fetching which will be inside parsedData.article
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false
+    });
+
+  }
+  //link for best react lifecycle method for component diagram: https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+  async componentDidMount() {
+    this.updateNews();
   }
 
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=7a53337011074519b2a9b032b4ed21b9&page=${this.state.page - 1}&pageSize=21`;
-
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles
-    });
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
   }
   handleNextClick = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 21)) { }
-    else {
-      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=7a53337011074519b2a9b032b4ed21b9&page=${this.state.page + 1}&pageSize=21`;
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles
-      });
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+      this.setState({ page: this.state.page + 1 });
+      this.updateNews();
     }
-
   }
-  
   render() {
     return (
       <>
         <div className="container my-4">
-          <h2> <strong>Newzilla</strong> - Today's Top Headlines </h2>
+          <h1 className="text-center"> <strong>Newzilla</strong> - Today's Top {`${(this.props.category.slice(0, 1)).toUpperCase()}${this.props.category.slice(1)}`} Headlines</h1>
+          {this.state.loading && <Spinner />}
           <div className="row">
-            {this.state.articles.map((element) => {
+            {!this.state.loading && this.state.articles.map((element) => {
               return (<div className="col-md-4 my-3" key={element.url}>
-                <NewsItem title={element.title ? element.title.slice(0, 45) : ''} description={element.description ? element.description.slice(0, 88) : ''} imageUrl={element.urlToImage ? element.urlToImage : 'https://m.files.bbci.co.uk/modules/bbc-morph-news-waf-page-meta/5.3.0/bbc_news_logo.png'} newsUrl={element.url} publishedAt={element.publishedAt} />
+                <NewsItem title={element.title ? element.title.slice(0, 45) : ''} description={element.description ? element.description.slice(0, 88) : ''} imageUrl={element.urlToImage ? element.urlToImage : 'https://m.files.bbci.co.uk/modules/bbc-morph-news-waf-page-meta/5.3.0/bbc_news_logo.png'} newsUrl={element.url} date={element.publishedAt} author={element.author} sourceName={element.source.name ? element.source.name : ''} />
               </div>);
             })}
           </div>
         </div>
         <div className="container d-flex justify-content-between my-3">
           <button className="btn btn-md btn-dark" disabled={this.state.page <= 1} onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button className="btn btn-md btn-dark" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 21)} onClick={this.handleNextClick}>Next &rarr; </button>
+          <button className="btn btn-md btn-dark" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} onClick={this.handleNextClick}>Next &rarr; </button>
         </div>
       </>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react'
+import plusSolid from '../plusSolid.svg'
 
 export class NewsItem extends Component {
-
+ constructor(){
+      super();
+      this.state = {
+        savedNewsArr: [],
+      }
+    } 
+     saveNews = (title, description, imageUrl, newsUrl, date, author, sourceName) => {
+      let savedNews = {
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        newsUrl: newsUrl,
+        date: date,
+        author: author,
+        sourceName: sourceName
+      }
+      //  this.setState({savedNewsArr: this.state.savedNewsArr.push(savedNews)});
+      console.log(this.state.savedNewsArr);
+    }
   render() {
-    let {title, description, imageUrl, newsUrl, publishedAt} = this.props;
+    let { title, description, imageUrl, newsUrl, date, author, sourceName } = this.props;
+   
+
+   
     return (
-      
-      <div className="card" title={`Date: ${publishedAt}`} style={{width: "18rem"}}>
-        <img src={imageUrl} style={{width: '286px',height: '178px'}}className="card-img-top" alt="..." />
+
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={imageUrl} style={{ width: '286px', height: '178px' }} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{title}...</h5>
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{
+            zIndex: '1',
+            left: '90%',
+            fontSize: 'small'
+          }}>
+            {sourceName}
+            <span className="visually-hidden">unread messages</span>
+          </span>
           <p className="card-text">{description}...</p>
-          <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+          <p className="card-text"><small>By {author ? author : 'Unknown'} on {new Date(date).toGMTString()} </small></p>
+          <div className="container d-flex justify-content-between">
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+            <img src={plusSolid} style={{ height: '30px', width: '50px' }} className={`btn btn-light ${newsUrl}`} onClick={() => {this.saveNews(title, description, imageUrl, newsUrl, date, author, sourceName)}}></img>
+          </div>
         </div>
       </div>
     )
