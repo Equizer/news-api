29077d5db110c7833a4400f0b68a49649a8a48c1
Equizer <130571773+Equizer@users.noreply.github.com>
2023-11-03 01:54:28 +0530

    change the project from class based component to function based component

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,91 +1,66 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner.js'
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: 'in',
-    pageSize: 8,
-    category: 'general'
-  }
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    apiKey: PropTypes.string,
-    category: PropTypes.string,
-  }
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: true,
-      page: 1,
-      totalResults: 0,
-    }
-    document.title = `${(this.props.category.slice(0, 1)).toUpperCase()}${this.props.category.slice(1)} - Newzilla`;
-  }
 
-  async updateNews() {
-    this.props.setProgress(10);
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true })
+const News = (props) => {
+
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  // document.title = `${(props.category.slice(0, 1)).toUpperCase()}${props.category.slice(1)} - Newzilla`;
+
+  const updateNews = async () => {
+    props.setProgressFunction(10)
+
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+
+    setLoading(true);
+
     let data = await fetch(url);
-    this.props.setProgress(30);
+    props.setProgressFunction(30);
     let parsedData = await data.json();
-    this.props.setProgress(70);
-    //following line will set the state of article to the article in the url we get from fetching which will be inside parsedData.article
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false
-    });
-    this.props.setProgress(100)
-  }
-  async componentDidMount() {
-    this.updateNews();
+    props.setProgressFunction(70);
+
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+
+    props.setProgressFunction(100)
   }
 
-  // handlePrevClick = async () => {
-  //   this.setState({ page: this.state.page - 1 });
-  //   this.updateNews();
-  // }
-  // handleNextClick = async () => {
-  //   if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-  //     this.setState({ page: this.state.page + 1 });
-  //     this.updateNews();
-  //   }
-  // }
+  useEffect(() => {
+    updateNews();
+  }, []);
 
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true })
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
     let parsedData = await data.json();
-    //following line will set the state of article to the article in the url we get from fetching which will be inside parsedData.article
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-      loading: false
-    });
+
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
   }
-  render() {
     return (
       <>
-        <h1 className="text-center"> <strong>Newzilla</strong> - Today's Top {`${(this.props.category.slice(0, 1)).toUpperCase()}${this.props.category.slice(1)}`} Headlines</h1>
+        <h1 className="text-center"> <strong>Newzilla</strong> - Today's Top {`${(props.category.slice(0, 1)).toUpperCase()}${props.category.slice(1)}`} Headlines</h1>
 
-        {this.state.loading && <Spinner />}
+        {loading && <Spinner />}
         <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length < this.state.totalResults}
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length < totalResults}
           loader={<Spinner />}
         >
           <div className="container">
             <div className="row">
-              {this.state.articles.map((element, index) => {
+              {articles.map((element, index) => {
                 return (<div className="col-md-4 my-3" key={element.url + index}>
                   <NewsItem title={element.title ? element.title.slice(0, 45) : ''} description={element.description ? element.description.slice(0, 88) : ''} imageUrl={element.urlToImage ? element.urlToImage : 'https://m.files.bbci.co.uk/modules/bbc-morph-news-waf-page-meta/5.3.0/bbc_news_logo.png'} newsUrl={element.url} date={element.publishedAt} author={element.author} sourceName={element.source.name ? element.source.name : ''} />
                 </div>);
@@ -93,15 +68,20 @@ export class News extends Component {
             </div>
           </div>
         </InfiniteScroll>
-
-
-        {/* <div className="container d-flex justify-content-between my-3">
-          <button className="btn btn-md btn-dark" disabled={this.state.page <= 1} onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button className="btn btn-md btn-dark" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} onClick={this.handleNextClick}>Next &rarr; </button>
-        </div> */}
       </>
     )
   }
+
+News.defaultProps = {
+  country: 'in',
+  pageSize: 8,
+  category: 'general'
+}
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  apiKey: PropTypes.string,
+  category: PropTypes.string,
 }
 
 export default News
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,35 +1,10 @@
 import React, { Component } from 'react'
 import plusSolid from '../plusSolid.svg'
 
-export class NewsItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savedNewsArr: [],
-    }
-  }
-  saveNews = (title, description, imageUrl, newsUrl, date, author, sourceName) => {
-    let savedNews = {
-      title: title,
-      description: description,
-      imageUrl: imageUrl,
-      newsUrl: newsUrl,
-      date: date,
-      author: author,
-      sourceName: sourceName
-    }
-    //  this.setState({savedNewsArr: this.state.savedNewsArr.push(savedNews)});
-    console.log(this.state.savedNewsArr);
-  }
-  render() {
-    let { title, description, imageUrl, newsUrl, date, author, sourceName } = this.props;
-
-
-
+const NewsItem = (props) =>  {
     return (
-
       <div className="card">
-        <img src={imageUrl} style={{height: '210px' }} className="card-img-top" alt="..." />
+        <img src={props.imageUrl} style={{height: '210px' }} className="card-img-top" alt="..." />
         <div className="card-body">
           <div  style={{
               display: 'flex',
@@ -38,19 +13,15 @@ export class NewsItem extends Component {
               right: 0,
               top: 0
             }}>
-            <span className="badge bg-danger"> {sourceName} </span>
+            <span className="badge bg-danger"> {props.sourceName} </span>
           </div>
 
-          <p className="card-text">{description}...</p>
-          <p className="card-text"><small>By {author ? author : 'Unknown'} on {new Date(date).toGMTString()} </small></p>
-          <div className="container d-flex justify-content-between">
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-            <img src={plusSolid} style={{ height: '30px', width: '50px' }} className={`btn btn-light ${newsUrl}`} onClick={() => { this.saveNews(title, description, imageUrl, newsUrl, date, author, sourceName) }}></img>
-          </div>
+          <p className="card-text">{props.description}...</p>
+          <p className="card-text"><small>By {props.author ? props.author : 'Unknown'} on {new Date(props.date).toGMTString()} </small></p>
+            <a href={props.newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
         </div>
       </div>
     )
-  }
 }
 
 export default NewsItem
